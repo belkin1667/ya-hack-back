e2727858dfc488f407d34a306b5b18d4b301fd48
Michael Belkin <belkinmike@yandex-team.ru>
2021-08-16 23:10:12 +0300

    Initial commit

---
+++ src/main/java/com/belkin/yahack/YaHackApplication.java
@@ -0,0 +1,13 @@
+package com.belkin.yahack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaHackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaHackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belkin/yahack/YaHackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belkin.yahack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaHackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
